f0b991617a2268ae1594eac0b5113b7db0951eca
Jordan Lai <jordan@Jordans-MacBook.local>
2019-03-20 12:15:49 +0800

    first commit

--- assets/main.js
+++ assets/main.js
@@ -10,21 +10,16 @@ ScrollReveal().reveal('.fact4', {delay: 2000, duration: 2000});
 var $overlay = $('<div id="overlay"></div>');
 var $image = $("<img>");
 
-//An image to overlay
 $overlay.append($image);
 
-//Add overlay
 $("body").append($overlay);
 
-  //click the image and a scaled version of the full size image will appear
   $("#photo-gallery a").click( function(event) {
     event.preventDefault();
     var imageLocation = $(this).attr("href");
 
-    //update overlay with the image linked in the link
     $image.attr("src", imageLocation);
 
-    //show the overlay
     $overlay.show();
   } );
 
